9030eaf08a6bee914ac3ba87d6dff7a19322040f
dzhoz0 <datuanho2k9@gmail.com>
2026-09-25 20:42:02 +0700

    update

--- nvim/lua/plugins/nvim_treesitter.lua
+++ nvim/lua/plugins/nvim_treesitter.lua
@@ -4,7 +4,7 @@ return {
         config = function () 
             local configs = require('nvim-treesitter.configs')
             configs.setup({
-                ensure_installed = {"cpp", "javascript"},
+                ensure_installed = {"cpp", "javascript", "python"},
                 sync_install = false,
                 highlight = {enable = true},
             })
